dad20206a6a2159867ba23baafcb89acbd7ef5d8
Lucky Rathore <luckyrathore03@gmail.com>
2020-01-29 22:58:32 +0530

    Updates

---
+++ precache-manifest.c493fea531b9107ca4baf2dd9d378d0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0cbc363d6d65c2b5f36f348978de1b8",
+    "url": "/abc-university/index.html"
+  },
+  {
+    "revision": "cacf1bd1c5aa909bc351",
+    "url": "/abc-university/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "23b36b6a0f6cf01ce242",
+    "url": "/abc-university/static/js/2.7307a793.chunk.js"
+  },
+  {
+    "revision": "13425e2e45fee351b4139959073d7e4f",
+    "url": "/abc-university/static/js/2.7307a793.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cacf1bd1c5aa909bc351",
+    "url": "/abc-university/static/js/main.49f049a1.chunk.js"
+  },
+  {
+    "revision": "45028c9812dc976c6ab9",
+    "url": "/abc-university/static/js/runtime-main.a50ae1d9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/abc-university/static/media/logo.5d5d9eef.svg"
+  }
+]);
