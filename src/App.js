3a6540bf87470daa5cd4e8faae64dcc1efb95483
Lucky Rathore <luckyrathore03@gmail.com>
2020-01-29 22:44:08 +0530

    gh installed

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Paperbase from './Paperbase';
+function App() {
+  return (
+    <Paperbase></Paperbase>
+  );
+}
+
+export default App;
