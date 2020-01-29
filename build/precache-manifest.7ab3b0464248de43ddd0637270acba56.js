e9eb1730ce1c577d5c2739ad731756d1407c4100
Lucky Rathore <luckyrathore03@gmail.com>
2020-01-29 20:50:58 +0530

    react code added

---
+++ build/precache-manifest.7ab3b0464248de43ddd0637270acba56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7db3267129a167e39e9ca08fbfa4904e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83ff38ee51960a5328fc",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "23b36b6a0f6cf01ce242",
+    "url": "/static/js/2.7307a793.chunk.js"
+  },
+  {
+    "revision": "13425e2e45fee351b4139959073d7e4f",
+    "url": "/static/js/2.7307a793.chunk.js.LICENSE"
+  },
+  {
+    "revision": "83ff38ee51960a5328fc",
+    "url": "/static/js/main.1b144875.chunk.js"
+  },
+  {
+    "revision": "a2d2423b8c0aaf73ef64",
+    "url": "/static/js/runtime-main.c8244578.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
